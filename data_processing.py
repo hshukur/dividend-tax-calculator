2ad97bde38acb3633b1b798bb99a2b5c0520ece4
hatamsh <hatam.shukur@gmail.com>
2023-05-28 22:40:02 +0200

    cleaned up the code and added docstring

--- data_processing.py
+++ data_processing.py
@@ -19,6 +19,8 @@ class DataProcessor:
 
     Methods
     -------
+    get_div_payment_dates()
+        gets the dividend transaction dates
     increase_delta_by_one()
         Increases self.delta by one
     day_before_transaction()
@@ -35,6 +37,14 @@ class DataProcessor:
         self.div_payment_data = {}
 
     def get_div_payment_dates(self):
+        """
+        Goes to https://investor.cisco.com to get the dividend payment date
+
+        Returns
+        -------
+        list
+            List that contains the dividend payment dates for the user-provided year
+        """
         URL = "https://investor.cisco.com/stock-information/dividends-and-splits/default.aspx"
         user_agent_header = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " \
                             "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
@@ -78,13 +88,6 @@ class DataProcessor:
         split_list = self.provided_date.split("-")
         provided_date = datetime.date(int(split_list[0]), int(split_list[1]), int(split_list[2]))
         new_delta_date = (provided_date - datetime.timedelta(days=self.delta)).isoformat()
-        return new_delta_date
-
-    def day_before_transaction_new(self, input_date):
-        provided_date = datetime.date(int(input_date[0]), int(input_date[1]), int(input_date[2]))
-        print(f"{provided_date}")
-        new_delta_date = (provided_date - datetime.timedelta(days=self.delta)).isoformat()
-        print(f"new {new_delta_date}")
         return new_delta_date
 
     def api_call_to_nbp(self):
@@ -133,41 +136,5 @@ class DataProcessor:
                     recv_404 = False
 
         usd_fx_rate = data_from_nbp.json()["rates"][0]["mid"]
-        print(f"The FX rate for {self.new_date} was {usd_fx_rate}")
-        return usd_fx_rate
-
-    def get_data_from_nbp_new(self, input_date):
-        """
-        Gets user-provided date, calculates delta, makes API call to NBP
-        API will return 404 in the case of lack of data for a correctly determined time interval
-        That can happen if we are trying to get the FX rate for a non-business day
-        In that case the while loop is invoked
-
-        Returns
-        -------
-        str
-            FX rate for USD
-        """
-
-        recv_404 = True
-
-        self.new_date = self.day_before_transaction_new(input_date)
-        data_from_nbp = self.api_call_to_nbp()
-        # print(data_from_nbp.status_code)
-
-        if data_from_nbp.status_code == 400:
-            # print(data_from_nbp.status_code)
-            return 400
-        if data_from_nbp.status_code == 404:
-            # print(data_from_nbp.status_code)
-            while recv_404:
-                self.increase_delta_by_one()
-                self.new_date = self.day_before_transaction()
-                data_from_nbp = self.api_call_to_nbp()
-                if data_from_nbp.status_code == 200:
-                    # print(data_from_nbp.status_code)
-                    recv_404 = False
-
-        usd_fx_rate = data_from_nbp.json()["rates"][0]["mid"]
-        print(f"The FX rate for {self.new_date} was {usd_fx_rate}")
+        # print(f"The FX rate for {self.new_date} was {usd_fx_rate}")
         return usd_fx_rate
